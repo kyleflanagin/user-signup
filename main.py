0854659761869227649f114700c5e33649879c76
Kyle Flanagin <Kyle Flanagin>
2017-04-06 18:00:35 -0500

    completed assignment

--- main.py
+++ main.py
@@ -14,12 +14,158 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
+import cgi
+import re
+
+
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+
+</head>
+<body style = "background-color:#DCDCDC">
+    <h1 style = "color:blue">
+        Signup
+    </h1>
+"""
+
+
+page_footer = """
+</body>
+</html>
+"""
+
+def make_form(values):
+    form="""
+    <form method="POST">
+        <label style="color:blue; ">Username:
+            <input style= "margin-left:41px" type="text" name="username" value="%(username)s" />
+            <span class="error">%(username_error)s</span>
+        </label>
+        <br>
+
+        <label style = "color:blue";>Password:
+            <input style= "margin-left:44px" type="password" name="password" value="%(password)s" />
+            <span class="error">%(password_error)s</span>
+        </label>
+        <br>
+
+        <label style = "color:blue";>Verify Password:
+            <input type="password" name="verify" value="%(verify)s" />
+            <span class="error">%(verify_error)s</span>
+        </label>
+        <br>
+
+        <label style = "color:blue"; >Email:
+            <input style= "margin-left:67px" type="text" name="email" value="%(email)s" />
+            <span class="error">%(email_error)s</span>
+        </label>
+        <br>
+
+        <input type="submit" />
+    </form>
+    """
+    return page_header + (form % values) + page_footer
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
+USER_PW = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return USER_PW.match(password)
+
+USER_EM = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    return USER_EM.match(email)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        values = {
+            'username': '',
+            'password': '',
+            'verify': '',
+            'email': '',
+            'username_error': '',
+            'password_error': '',
+            'verify_error': '',
+            'email_error': ''
+        }
+        self.response.write(make_form(values))
+
+    def post(self):
+        have_error = False
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+        if not username:
+            username = ''
+        if not password:
+            password = ''
+        if not verify:
+            verify = ''
+        if not email:
+            email = ''
+
+        values = {
+            'username': username,
+            'password': password,
+            'verify': verify,
+            'email': email,
+            'username_error': '',
+            'password_error': '',
+            'verify_error': '',
+            'email_error': ''
+        }
+
+        if username.isspace() or (username.strip() == ""):
+            values['username_error'] = "Please enter a user name"
+            have_error = True
+        else:
+            if not valid_username(username):
+                values['username_error'] = "Please enter valid user name"
+                have_error = True
+
+        if not valid_password(password):
+            values['password'] = ''
+            values['password_error'] = "Please enter valid password"
+            have_error = True
+
+        if not valid_password(verify):
+            values['verify'] = ''
+            values['verify_error'] = "Please enter valid password"
+            have_error = True
+        else:
+            if verify != password:
+                values['password'] = ''
+                values['verify'] = ''
+                values['verify_error'] = "Verify password doesn't match password"
+                have_error = True
+
+        if (not email.isspace()) and (email.strip() != ""):
+            if not valid_email(email):
+                values['email_error'] = "Please enter valid email"
+                have_error = True
+
+        if have_error:
+            self.response.write(make_form(values))
+        else:
+            self.redirect('/welcome?username=' + username)
+
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get("username")
+        if valid_username(username):
+            self.response.write("<h2>Welcome, " + username + "</h2>")
+        else:
+            self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
